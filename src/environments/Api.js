c7a80431982f14e48d608c401002f06fae993b63
rosyrosie <kshunn@snu.ac.kr>
2022-02-24 21:48:04 +0900

    feat: implement login

--- src/environments/Api.js
+++ src/environments/Api.js
@@ -1,5 +1,9 @@
 const BASE_URL = 'https://test.divo.kr/';
 
+
+export const LOGIN_URL = BASE_URL + 'auth/login/';
+export const AUTH_URL = BASE_URL + 'auth/register/message/';
+
 export const SA_AMOUNT_URL = BASE_URL + 'keyword/amount?keyword=';
 export const SA_CONTENT_URL = BASE_URL + 'keyword/contents?keyword=';
 export const SA_CHART_URL = BASE_URL + 'keyword/chart?keyword=';
--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,18 +1,46 @@
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from '../components/Header';
+import axios from 'axios';
+import { LOGIN_URL } from '../environments/Api';
 
 export default function Login(){
 
   const navigate = useNavigate();
+
+  const [ id, setId ] = useState('');
+  const [ pwd, setPwd ] = useState('');
+
+  const login = e => {
+    if(e.key !== 'Enter') return;
+    const body = {
+      email: id,
+      password: pwd
+    }
+    axios.post(LOGIN_URL, body).then(res => {
+      if(res.data.token){
+        localStorage.clear();
+        localStorage.setItem('token', res.data.token);
+        navigate('/');
+      }
+      else{
+        alert('일치하는 계정이 없습니다');
+        setPwd('');
+        localStorage.clear();
+      }
+    }).catch(e => {
+      console.log('POST Error');
+    });
+  }
 
   return (
     <S.Body>
       <Header />
       <S.Content>
         <S.Text>로그인하기</S.Text>
-        <S.Input placeholder="이메일 또는 전화번호"/>
-        <S.Input placeholder="비밀번호" type="password"/>
+        <S.Input placeholder="이메일 또는 전화번호" type="email" value={id} onChange={e => setId(e.target.value)} />
+        <S.Input placeholder="비밀번호" type="password" value={pwd} onChange={e => setPwd(e.target.value)} onKeyPress={login} />
         <S.SignUp>회원이 아니신가요?<S.Blue onClick={() => navigate('/signup')}>회원가입하기</S.Blue></S.SignUp>
       </S.Content>
       <S.Content />
